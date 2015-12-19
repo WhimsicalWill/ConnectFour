29f0d1624e3862207672f54740eb517f0449fe6a
WhimsicalWill <knipewilliam20@gmail.com>
2015-12-18 20:55:42 -0500

    Add Version Number Log Message
    For easier understanding of which version the gh-pages website is
    showing.

--- Game.js
+++ Game.js
@@ -5,6 +5,7 @@ var pieceLength = Math.floor(gameHeight / 7);
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '', {create: create, update: update});
 
 function create() {
+    console.log("version 1.337");
     //Center the phaser game
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
